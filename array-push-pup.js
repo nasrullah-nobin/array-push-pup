f0b0b87a6dda4f0d810dd6a280c37c103d3d5257
nasrullah-nobin <nasrullahbd82@gmail.com>
2026-01-30 16:01:46 +0600

    add advanced array method examples

--- array-push-pup.js
+++ array-push-pup.js
@@ -13,11 +13,42 @@ number.shift();
 friend.shift();
 console.log(number, friend);
 
-
 // pop ba shift kore je element ta kete dewa hoi seta ke o ark ta variable e joma rakha jai
 
-const num = [3,4,3,1,7,5];
-const fr = ['roshid', 'jamil', 'korim'];
+const num = [1, 2, 3, 4, 5, 6];
+const fr = ["roshid", "jamil", "korim"];
 const box1 = num.shift();
 const box2 = fr.shift();
 console.log(box1, box2);
+
+// includes ati check kore Array er modde niddisto upadhan ache ki na?
+console.log(num.includes(4));
+console.log(fr.includes("jamil"));
+
+// Array er upadhan koita seta check kora jai length dia
+console.log(num.length);
+
+// Array.isArray dia check kora jai je property ta Array ki na
+console.log(Array.isArray(num));
+const mission = "i want become a software developer";
+console.log(Array.isArray(mission));
+
+// indexof ati dia Array er opadhan er index check kora jai
+console.log(num.indexOf(5));
+
+// Array theke string e convert korte use hoi join method
+console.log(num.join(" "));
+
+// concat methhod use hoi 2 ta Array ke ekta array bananor kaje
+console.log(num.concat(fr));
+
+// slice ata Array/string er kono ekta aungsho ke vag kore dai
+const learn = "I am learning web dev";
+const result = learn.slice(0, 4);
+console.log(result);
+
+// splice() array-এর ভেতর থেকে element remove / add / replace করে এবং মূল array পরিবর্তন করে → example:
+let arr = [10, 20, 30];
+console.log(arr);
+arr.splice(1, 1, 99); 
+console.log(arr);
